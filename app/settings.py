9bb6326da30ffe129110eeb246954cd562e89b1f
Radomir Niewiadomski <radomirn96@gmail.com>
2025-11-30 13:01:34 +0100

    feat(menu): init menu app and configure media files
    - Initialize the 'menu' django app.
    - Configure MEDIA_URL and MEDIA_ROOT in settings to handle user-uploaded content (dish images).
    - Update root urls.py to serve media files in debug mode.

--- app/settings.py
+++ app/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "user",
+    "menu",
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@
 URL configuration for app project.
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -12,3 +14,6 @@ urlpatterns = [
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="api-schema"), name="api-docs"),
     path("api/user/", include("user.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
